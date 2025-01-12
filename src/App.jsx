ebc71b9d043be79b32a5b73b2f3c20ea014e2d4b
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-01-12 07:39:19 +0100

    finsh roles

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 import Home from "./pages/home/Home";
 import Dashboard from "./pages/dashboard/Dashboard";
-import User from "./pages/userPages/User";
-import Admin from "./pages/adminPages/Admin";
 import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
@@ -18,8 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/admin" element={<Admin />} />
+ 
           </Route>
         </Routes>
       </AuthProvider>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,22 +1,15 @@
 import { useAuth } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import Admin from "../adminPages/Admin";
+import User from "../userPages/User";
 
 const Dashboard = () => {
     const { role } = useAuth()
-    const navigate = useNavigate()
+    
 
-    useEffect(() => {
-        if (role === "admin") {
-            navigate("/admin")
-        } else if (role === "user") {
-            navigate("/user")
-        }
-    }, [role, navigate])
 
     return (
         <div>
-            <h1 className="text-center text-2xl font-bold">loading...</h1>
+            {role === "admin" ? <Admin /> : <User />}
         </div>
     )
 }
